15457abcc1305dbd05395698ac5169739505ddb5
4wa4k3 <andres@ajvelasquez.com>
2020-04-21 12:14:43 -0400

    Added options to .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,9 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/return-in-computed-property': 'off',
+    'vue/no-use-v-if-with-v-for': 'off'
   }
 }
